b9800aeb4e30ba633b483e5adc0dfa8696796da2
Vladimír Hucovič <hucovic.vladimir@gmail.com>
2022-04-19 18:53:03 +0200

    Added some more tests, removed a redundant test

--- src/mathtest/test.cpp
+++ src/mathtest/test.cpp
@@ -29,26 +29,30 @@ protected:
 
 TEST_F(testCase1, addTest){
 	EXPECT_TRUE(SteamMath :: sum(a, b) == 3.0);
+	EXPECT_TRUE(SteamMath :: sum(a, -5) == -3.5);
 }
 
 TEST_F(testCase1, subTest){
-	EXPECT_TRUE(SteamMath :: sub(a, b) == 0.0);
+EXPECT_TRUE(SteamMath :: sub(a, b) == 0.0);
+EXPECT_TRUE(SteamMath :: sub(a, -5) == 6.5);
 }
 
 TEST_F(testCase1, mulTest){
 	EXPECT_TRUE(SteamMath :: mul(a, b) == 2.25);
 	EXPECT_TRUE(SteamMath :: mul(a, 0.0) == 0.0);
+	EXPECT_TRUE(SteamMath :: mul(a, -2) == -3);
 }
 
 TEST_F(testCase1, divTest){
 	EXPECT_TRUE(SteamMath :: div(a, b) == 1);
+	EXPECT_TRUE(SteamMath :: div(a, -1) == -a);
 	EXPECT_THROW(SteamMath :: div(a, 0.0), sm :: DivisionByZero);
 }
 
 TEST_F(testCase2, factTest){
 	EXPECT_TRUE(SteamMath :: fact(a) == 24);
 	EXPECT_TRUE(SteamMath :: fact(b) == 1);
-	EXPECT_THROW(SteamMath :: fact(-1), sm::PositiveNonZeroNumberRequired);
+	EXPECT_THROW(SteamMath :: fact(-1), sm:: OutputUndefined);
 }
 
 TEST_F(testCase1, powTest){
@@ -57,17 +61,16 @@ TEST_F(testCase1, powTest){
 }
 
 TEST_F(testCase2, rootTest){
-	EXPECT_TRUE(SteamMath :: root(a, 2.0) == 2);
-	EXPECT_TRUE(SteamMath :: root(b, 3.0) == 2);
+	EXPECT_TRUE(SteamMath :: root(a, 2) == 2);
+	EXPECT_TRUE(SteamMath :: root(b, 3) == 2);
+	double result = SteamMath :: root(b, -2);
+	EXPECT_TRUE(std::abs(result) - 0.353553390593273 < 1e-15);
+	EXPECT_THROW(SteamMath :: root(a, 0), sm :: OutputUndefined);
+	EXPECT_THROW(SteamMath :: root(-4, 2), sm :: OutputUndefined);
 }
-
-TEST_F(testCase2, divTest){
-	EXPECT_THROW(SteamMath :: div(a, 0.0), sm :: DivisionByZero);
-}
-
 
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  	::testing::InitGoogleTest(&argc, argv);
+  	return RUN_ALL_TESTS();
 }
