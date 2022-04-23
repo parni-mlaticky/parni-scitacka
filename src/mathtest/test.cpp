0ef4063187d8e4b11e48192304699eb788d517d7
Vladimír Hucovič <hucovic.vladimir@gmail.com>
2022-04-23 15:56:07 +0200

    Removed test fixtures because they were honestly kind of useless here

--- src/mathtest/test.cpp
+++ src/mathtest/test.cpp
@@ -10,70 +10,49 @@
 
 using namespace sm;
 
-class doubles : public :: testing ::Test{
-protected:
-	virtual void SetUp(){
-		a = 1.5;
-		b = 1.5;
-	}
-	double a;
-	double b;
-};
-
-class integers : public :: testing ::Test{
-protected:
-	virtual void SetUp(){
-		a = 4;
-		b = 8;
-	}
-	int a;
-	int b;
-};
-
-
-TEST_F(doubles, addTest){
-	EXPECT_TRUE(SteamMath::sum(a, b) == 3.0);
-	EXPECT_TRUE(SteamMath::sum(a, -5) == -3.5);
+TEST(basic, addTest){
+	EXPECT_TRUE(SteamMath::sum(1.5, 1.5) == 3.0);
+	EXPECT_TRUE(SteamMath::sum(1.5, -5) == -3.5);
 }
 
-TEST_F(doubles, subTest){
-EXPECT_TRUE(SteamMath::sub(a, b) == 0.0);
-EXPECT_TRUE(SteamMath::sub(a, -5) == 6.5);
+TEST(basic, subTest){
+	EXPECT_TRUE(SteamMath::sub(1.5, 1.5) == 0.0);
+	EXPECT_TRUE(SteamMath::sub(1.5, -5) == 6.5);
 }
 
-TEST_F(doubles, mulTest){
-	EXPECT_TRUE(SteamMath::mul(a, b) == 2.25);
-	EXPECT_TRUE(SteamMath::mul(a, 0.0) == 0.0);
-	EXPECT_TRUE(SteamMath::mul(a, -2) == -3);
+TEST(basic, mulTest){
+	EXPECT_TRUE(SteamMath::mul(1.5, 1.5) == 2.25);
+	EXPECT_TRUE(SteamMath::mul(1.5, 0.0) == 0.0);
+	EXPECT_TRUE(SteamMath::mul(1.5, -2) == -3);
 }
 
-TEST_F(doubles, divTest){
-	EXPECT_TRUE(SteamMath::div(a, b) == 1);
-	EXPECT_TRUE(SteamMath::div(a, -1) == -a);
-	EXPECT_THROW(SteamMath::div(a, 0.0), sm::DivisionByZero);
+TEST(basic, divTest){
+	EXPECT_TRUE(SteamMath::div(1.5, 1.5) == 1);
+	EXPECT_TRUE(SteamMath::div(1.5, -1) == -1.5);
+	EXPECT_THROW(SteamMath::div(1.5, 0.0), sm::DivisionByZero);
 }
 
-TEST_F(integers, factTest){
-	EXPECT_TRUE(SteamMath::fact(a) == 24);
-	EXPECT_TRUE(SteamMath::fact(b) == 40320);
+TEST(advanced, factTest){
+	EXPECT_TRUE(SteamMath::fact(4) == 24);
+	EXPECT_TRUE(SteamMath::fact(8) == 40320);
 	EXPECT_THROW(SteamMath::fact(-1), sm::OutputUndefined);
 }
 
-TEST_F(doubles, powTest){
-	EXPECT_TRUE(SteamMath::pow(a, 2) == 2.25);
-	EXPECT_TRUE(SteamMath::pow(a, 0.0) == 1);
+TEST(advanced, powTest){
+	EXPECT_TRUE(SteamMath::pow(1.5, 2) == 2.25);
+	EXPECT_TRUE(SteamMath::pow(1.5, 0.0) == 1);
 }
 
-TEST_F(integers, rootTest){
-	EXPECT_TRUE(SteamMath::root(a, 2) == 2);
-	EXPECT_TRUE(SteamMath::root(b, 3) == 2);
-	double result = SteamMath::root(b, -2);
+TEST(advanced, rootTest){
+	EXPECT_TRUE(SteamMath::root(4, 2) == 2);
+	EXPECT_TRUE(SteamMath::root(8, 3) == 2);
+	double result = SteamMath::root(8, -2);
 	EXPECT_TRUE(std::abs(result) - 0.353553390593273 < EPSILON);
-	EXPECT_THROW(SteamMath::root(a, 0), sm::OutputUndefined);
+	EXPECT_THROW(SteamMath::root(4, 0), sm::OutputUndefined);
 	EXPECT_THROW(SteamMath::root(-4, 2), sm::OutputUndefined);
 }
 
-TEST_F(doubles, quadRootTest){
+TEST(advanced, quadRootTest){
 	std::vector<double> rootVector;
 	rootVector = SteamMath::quadRoot(1, 3, 2);
 	EXPECT_TRUE(rootVector.at(0) == -1.0 && rootVector.at(1) == -2.0);
@@ -98,7 +77,7 @@ TEST_F(doubles, quadRootTest){
 	EXPECT_THROW(SteamMath::quadRoot(3.0, 1.0, 5.0), sm::OutputUndefined);
 }
 
-TEST(goniometric, sine){
+TEST(goniometric, sineTest){
 	EXPECT_TRUE(std::abs(SteamMath::sin(1) - 0.841470984807896) < EPSILON);
 	EXPECT_TRUE(std::abs(SteamMath::sin(M_PI)) < EPSILON);
 	EXPECT_TRUE(SteamMath::sin(0) == 0);
