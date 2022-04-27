9b26e2866e9f835dc33bdaf0e90de94717212ca3
Marek Havel <git@havy.eu>
2022-04-28 00:08:23 +0200

    Basic installer/uninstaller, not yet tested

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt install git qtdeclarative5-dev qt5-qmake g++
+#cd (adresar-programu)/src
+make
+sudo cp ../build/gui/gui /usr/bin/parni-scitacka
+sudo chmod a+x /usr/bin/parni-scitacka
+cp meta/parni-scitacka.desktop ~/.local/share/applications
+sudo cp meta/parni-scitacka.xpm /usr/share/pixmaps/
---
+++ uninstaller.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo rm /usr/bin/parni-scitacka
+rm ~/.local/share/applications/parni-scitacka.desktop
+sudo rm /usr/share/pixmaps/parni-scitacka.xpm
