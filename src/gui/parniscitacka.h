edcc93c67ce4693f71fb3664a8b1776b17512fd4
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2022-04-28 23:24:00 +0200

    Fixed double definition in PS.h

--- src/gui/parniscitacka.h
+++ src/gui/parniscitacka.h
@@ -183,8 +183,6 @@ private slots:
 	 */
     void on_ComboBox_index_changed(int value);
 
-    void on_actionPrecision_triggered();
-
 protected:
 	/**
 	 * @brief This function is called whenever a keyboard button is pressed and enables the user to control the app
